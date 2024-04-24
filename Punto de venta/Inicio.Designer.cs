803bf8186dc87a8636caf19c31fe3b1479210303
Jon <Jon@DESKTOP-2AIPO1P>
2024-04-24 11:27:32 -0600

    Agregar archivos de proyecto.

---
+++ Punto de venta/Inicio.Designer.cs
@@ -0,0 +1,78 @@
+﻿namespace Punto_de_venta
+{
+    partial class Inicio
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuTitulo = new System.Windows.Forms.MenuStrip();
+            this.menu = new System.Windows.Forms.MenuStrip();
+            this.SuspendLayout();
+            // 
+            // menuTitulo
+            // 
+            this.menuTitulo.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menuTitulo.Location = new System.Drawing.Point(0, 48);
+            this.menuTitulo.Name = "menuTitulo";
+            this.menuTitulo.Size = new System.Drawing.Size(1215, 28);
+            this.menuTitulo.TabIndex = 0;
+            this.menuTitulo.Text = "menuStrip1";
+            // 
+            // menu
+            // 
+            this.menu.AutoSize = false;
+            this.menu.BackColor = System.Drawing.Color.SteelBlue;
+            this.menu.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.menu.Location = new System.Drawing.Point(0, 0);
+            this.menu.Name = "menu";
+            this.menu.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.menu.Size = new System.Drawing.Size(1215, 48);
+            this.menu.TabIndex = 1;
+            this.menu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.menuStrip2_ItemClicked);
+            // 
+            // Inicio
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1215, 465);
+            this.Controls.Add(this.menuTitulo);
+            this.Controls.Add(this.menu);
+            this.MainMenuStrip = this.menuTitulo;
+            this.Name = "Inicio";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Inicio_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuTitulo;
+        private System.Windows.Forms.MenuStrip menu;
+    }
+}
+
---
+++ Punto de venta/Inicio.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Punto_de_venta
+{
+    public partial class Inicio : Form
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void Inicio_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void menuStrip2_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+
+        }
+    }
+}
