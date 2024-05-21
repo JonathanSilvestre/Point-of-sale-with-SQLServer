5e0339bf10b8571cf5839e29ff2ba37ae81ce983
Diego RD <diegorico1500@gmail.com>
2024-05-21 09:59:51 -0600

    Diseño de ventas terminado

--- Punto de venta/FRMVentas.cs
+++ Punto de venta/FRMVentas.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Punto_de_venta.Utilidades;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -19,6 +20,26 @@ namespace Punto_de_venta
         }
 
         private void FRMVentas_Load(object sender, EventArgs e)
+        {
+            cbotipodocumento.Items.Add(new OpcionCombo() { Valor = "Boleta", Texto = "Boleta" });
+            cbotipodocumento.Items.Add(new OpcionCombo() { Valor = "Factura", Texto = "Factura" });
+            cbotipodocumento.DisplayMember = "Texto";
+            cbotipodocumento.ValueMember = "Valor";
+            cbotipodocumento.SelectedIndex = 0;
+
+            txtfecha.Text = DateTime.Now.ToString("dd/MM/yyyy");
+            txtidproducto.Text = "0";
+
+            txtpagacon.Text = "";
+            txttotalpagar.Text = "0";
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label8_Click(object sender, EventArgs e)
         {
 
         }
