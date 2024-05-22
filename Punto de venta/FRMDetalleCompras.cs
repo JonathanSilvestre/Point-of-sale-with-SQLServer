cc1c02014986bb0b86cff02b5a4a0e3ea0f01f6b
Jonathan Silvestre Alvarez Cruz <jhon_ale_hijos@hotmail.com>
2024-05-21 21:28:06 -0600

    Bajar pdf de detalle compra terminado

--- Punto de venta/FRMDetalleCompras.cs
+++ Punto de venta/FRMDetalleCompras.cs
@@ -1,10 +1,14 @@
 ﻿using CapaEntidad;
 using CapaNegocio;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -70,70 +74,70 @@ namespace Punto_de_venta
             }
 
             string Texto_Html = Properties.Resources.PlantillaCompra.ToString();
-            /*Aqui se agrega negocio
-             * Negocio oDatos = new CN_Negocio().ObtenerDatos();
-             * Texto_Html = Texto_Html.Replace("@nombrenegocio", oDatos.Nombre.ToUpper());
-             * Texto_Html = Texto_Html.Replace("@docnegocio",oDatos.RUC);
-             * Texto_Html = Texto_Html.Replace("@direcnegocio",oDatos.Direccion);
-             * 
-             * Texto_Html = Texto_Html.Replace("@tipodocumento",txttipodocumentocompra.Text.ToUpper();
-             * Texto_Html = Texto_Html.Replace("@numerodocumento",txtnumerodocumento.Text);
-             * 
-             * Texto_Html = Texto_Html.Replace("@docproveedor",txtdocproveedor.Text);
-             * Texto_Html = Texto_Html.Replace("@nombreproveedor",txtnombreproveedor.Text);
-             * Texto_Html = Texto_Html.Replace("@fecharegistro",txtfecha.Text);
-             * Texto_Html = Texto_Html.Replace("@usuarioregistro",txtusuario.Text);
-             * 
-             * string filas = string empty;
-             * foreach(DataGridViewRow row in dgvdata.Rows)
-             * {
-             *      filas += "<tr>";
-             *      filas += "<td>" + row.Cells["Producto"].Value.ToString() + "</td>";
-             *      filas += "<td>" + row.Cells["PrecioCompra"].Value.ToString() + "</td>";
-             *      filas += "<td>" + row.Cells["Producto"].Value.ToString() + "</td>";
-             *      filas += "<td>" + row.Cells["Producto"].Value.ToString() + "</td>";
-             *      filas += "</tr>";
-             * }
-             * 
-             * Texto_Html = Texto_Html.Replace("@filas",filas);
-             * Texto_Html = Texto_Html.Replace("@montototal",txtmontotoal.Text);
-             * 
-             * SaveFileDialog savefile = new SaveFileDialog();
-             * savefile.FileName = string.Format("Compra_{0}.pdf",txtnumerodocumento.Text);
-             * savefile.Filter = "Pdf files |*.pdf";
-             * if(savefile.ShowDialog() == DialogResult.OK) 
-             * { 
-             *      using (FileStream stream = new FileStream(savefile.FileName,FileMode.Create))
-             *      {
-             *          Document pdfDoc = new Document(PagSize.A4,25,25,25,25);
-             *          
-             *          PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
-             *          pdfDoc.Open();
-             *          
-             *          bool obtenido = true;
-             *          byte[] byteImage = new CN_Negocio().ObtenerLogo(out obtenido);
-             *          
-             *          if(obtenido)
-             *          {
-             *              iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(byteImage);
-             *              img.ScaleToFit(60,60);
-             *              img.Alignment = iTextSharp.text.Image.UNDERLYING;
-             *              img.SetAbsolutePosition(pdfDoc.Left,pdfDoc.GetTop(51));
-             *              pdfDoc.Add(img);
-             *          }
-             *          
-             *          using(StringReader sr = new StringReader(Texto_Html))
-             *          {
-             *              XMLWorkerHelper.GetInstance().ParseXHtml(writer,pdfDoc,sr);
-             *          }
-             *          
-             *          pdfDoc.Close();
-             *          stream.Close();
-             *          MassageBox.Show("Documento Generado","Mensaje",MassageBoxButtons.OK,MassageBoxIcon.Infomation)
-             *          
-             *      }
-             * }
-            */
+
+             Negocio oDatos = new CN_Negocio().ObtenerDatos();
+             Texto_Html = Texto_Html.Replace("@nombrenegocio", oDatos.NombreNegocio.ToUpper());
+             Texto_Html = Texto_Html.Replace("@docnegocio",oDatos.RUC);
+             Texto_Html = Texto_Html.Replace("@direcnegocio",oDatos.Direccion);
+             
+             Texto_Html = Texto_Html.Replace("@tipodocumento",txttipodocumentocompra.Text.ToUpper());
+             Texto_Html = Texto_Html.Replace("@numerodocumento",txtnumerodocumento.Text);
+              
+             Texto_Html = Texto_Html.Replace("@docproveedor",txtdocproveedor.Text);
+             Texto_Html = Texto_Html.Replace("@nombreproveedor",txtnombreproveedor.Text);
+             Texto_Html = Texto_Html.Replace("@fecharegistro",txtfecha.Text);
+             Texto_Html = Texto_Html.Replace("@usuarioregistro",txtusuario.Text);
+              
+             string filas = string.Empty;
+             foreach(DataGridViewRow row in dgvdata.Rows)
+             {
+                  filas += "<tr>";
+                  filas += "<td>" + row.Cells["Producto"].Value.ToString() + "</td>";
+                  filas += "<td>" + row.Cells["Precio_Compra"].Value.ToString() + "</td>";
+                  filas += "<td>" + row.Cells["Producto"].Value.ToString() + "</td>";
+                  filas += "<td>" + row.Cells["Producto"].Value.ToString() + "</td>";
+                  filas += "</tr>";
+             }
+             
+             Texto_Html = Texto_Html.Replace("@filas",filas);
+             Texto_Html = Texto_Html.Replace("@montototal",txtmontototal.Text);
+             
+             SaveFileDialog savefile = new SaveFileDialog();
+             savefile.FileName = string.Format("Compra_{0}.pdf",txtnumerodocumento.Text);
+             savefile.Filter = "Pdf files |*.pdf";
+             if(savefile.ShowDialog() == DialogResult.OK) 
+             { 
+                  using (FileStream stream = new FileStream(savefile.FileName,FileMode.Create))
+                  {
+                      Document pdfDoc = new Document(PageSize.A4,25,25,25,25);
+                      
+                      PdfWriter writer = PdfWriter.GetInstance(pdfDoc, stream);
+                      pdfDoc.Open();
+                     
+                      bool obtenido = true;
+                      byte[] byteImage = new CN_Negocio().ObtenerLogo(out obtenido);
+                       
+                      if(obtenido)
+                      {
+                          iTextSharp.text.Image img = iTextSharp.text.Image.GetInstance(byteImage);
+                          img.ScaleToFit(60,60);
+                          img.Alignment = iTextSharp.text.Image.UNDERLYING;
+                          img.SetAbsolutePosition(pdfDoc.Left,pdfDoc.GetTop(51));
+                          pdfDoc.Add(img);
+                      }
+                      
+                      using(StringReader sr = new StringReader(Texto_Html))
+                      {
+                          XMLWorkerHelper.GetInstance().ParseXHtml(writer,pdfDoc,sr);
+                      }
+                      
+                      pdfDoc.Close();
+                      stream.Close();
+                      MessageBox.Show("Documento Generado", "Mensaje", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+                  }
+             }
+            
         }
     }
 }
